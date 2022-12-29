f7f0f412113a2f38320b15115ed823a7d41c6910
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-12-29 07:45:22 +0000

    smart solution

--- my_solutions/palindrome.py
+++ my_solutions/palindrome.py
@@ -6,23 +6,28 @@ cleaned_word = re.sub('\W+','',check_word)
 # print(cleaned_word) - check
 # print(cleaned_word[2]) - check
 
-def check_palindrome(word):
-    word_length = len(word)
-    check_upto = math.ceil(word_length/2)
-    is_palindrome = ''
-    # print(word_length) - check
-    for i in range(0, check_upto):
-        first_half_letter = word[i]
-        second_half_letter = word[word_length-i-1]
-        # print(first_half_letter,second_half_letter ) - check
-        if first_half_letter != second_half_letter:
-            is_palindrome = False
-            break    
-        elif first_half_letter == second_half_letter:
-            if i == check_upto-1:
-                is_palindrome = True
-            else:
-                pass
-    print(is_palindrome)
+# def check_palindrome(word):
+#     word_length = len(word)
+#     check_upto = math.ceil(word_length/2)
+#     is_palindrome = ''
+#     # print(word_length) - check
+#     for i in range(0, check_upto):
+#         first_half_letter = word[i]
+#         second_half_letter = word[word_length-i-1]
+#         # print(first_half_letter,second_half_letter ) - check
+#         if first_half_letter != second_half_letter:
+#             is_palindrome = False
+#             break    
+#         elif first_half_letter == second_half_letter:
+#             if i == check_upto-1:
+#                 is_palindrome = True
+#             else:
+#                 pass
+#     print(is_palindrome)
 
-check_palindrome(cleaned_word)
+def check_palindrome(word):
+    forward = word
+    backward = word[::-1]
+    return forward == backward
+
+print(check_palindrome(cleaned_word))
